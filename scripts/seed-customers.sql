ab31af86c236260f2720bee35cecf4d4848211ac
Bogdan D <bogdan776@gmail.com>
2025-09-26 17:43:43 +0300

    Fix schema path and add pagila dump

---
+++ scripts/seed-customers.sql
@@ -0,0 +1,4 @@
+INSERT INTO customer (customer_id, store_id, first_name, last_name, email, address_id, activebool, create_date)
+VALUES
+  (1, 1, 'John', 'Doe', 'john.doe@example.com', 1, true, CURRENT_DATE),
+  (2, 1, 'Jane', 'Smith', 'jane.smith@example.com', 2, true, CURRENT_DATE);
