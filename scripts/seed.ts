5f31d20eee80239271acac59aeb95d7bcef4d185
Bogdan D <bogdan776@gmail.com>
2025-09-27 00:34:42 +0300

    Fix schema path and add pagila dump

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -12,7 +12,7 @@ const client = new Client({ connectionString: process.env.DB_URL });
       customer_id, store_id, first_name, last_name, email, address_id, active, create_date
     )
     VALUES (
-      1, 1, 'Ситников', 'Митя', 'MITIASITNIKOV@teleworm.us', 576, true, '2023-03-06T21:44:21.000Z'
+      1, 1, 'Ситников', 'Митя', 'MITIASITNIKOV@teleworm.us', 576, 1, '2023-03-06T21:44:21.000Z'
     )
     ON CONFLICT (customer_id) DO NOTHING;
   `);
